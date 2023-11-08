429bb2389f08bc6c2463980b36d454cd974fcf9b
flan6 <flander_abreu@hotmail.com>
2023-11-08 04:52:47 -0300

    feat(linkedlist): add basic tests

---
+++ data_structures/linked_list_test.go
@@ -0,0 +1,55 @@
+package data_structures_test
+
+import (
+	"testing"
+
+	ds "github.com/flan6/data-algo/data_structures"
+)
+
+func TestLinkedList(t *testing.T) {
+	// Setup
+	list := ds.NewLinkedList[int]()
+
+	val := []int{0, -10, 69, 420}
+	for i := range val {
+		list.Append(val[i])
+	}
+
+	// Length
+	expectedSize := len(val)
+	listSize := list.Length()
+	if expectedSize != int(listSize) {
+		t.Fatalf("Length: wrong size for the list, expected %d got %d", expectedSize, listSize)
+	}
+
+	// Get and Append from setup
+	for i := range val {
+		res, err := list.Get(uint(i))
+		if res != val[i] || err != nil {
+			t.Fatalf("Append: got %d expected %d at index %d", res, val[i], i)
+		}
+	}
+
+	// Preappend
+	v := 69420
+	list.Preappend(v)
+	if res, err := list.Get(0); res != v || err != nil {
+		t.Fatalf("Preappend: got %d but expected %d at head", res, v)
+	}
+
+	// IndexOf
+	idx := list.IndexOf(v)
+	if idx != 0 {
+		t.Fatalf("IndexOf: got index %d but expected 0 for value %d", idx, v)
+	}
+
+	// Set
+	v = 123
+	err := list.Set(0, v)
+	if err != nil {
+		t.Fatalf("Set: coult not set %d at index 0, got err %s", v, err)
+	}
+	if res, err := list.Get(0); res != v || err != nil {
+		t.Fatalf("Set: got %d but expected %d at head", res, v)
+	}
+}
