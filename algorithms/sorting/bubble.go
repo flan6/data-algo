22abbd7ac72d571b6a825bb39857254d069a8d55
flan6 <flander_abreu@hotmail.com>
2024-03-31 22:42:24 -0300

    feat(algo/sorting): bubble sort

--- algorithms/sorting/bubble.go
+++ algorithms/sorting/bubble.go
@@ -1,3 +1,20 @@
 package sorting
 
-func BubbleSort[E ~[]T, T any](s E) {}
+import "cmp"
+
+func BubbleSort[E ~[]T, T cmp.Ordered](s E) {
+	for i := 0; i < len(s)-1; i++ {
+		swapped := false
+
+		for j := 0; j < len(s)-i-1; j++ {
+			if s[j] > s[j+1] {
+				swap(&s[j], &s[j+1])
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			break
+		}
+	}
+}
